20150d8c62b2810a41cffd41ada95ff4168d5e1f
Carlos Brandao <cefb004@gmail.com>
2025-11-12 14:21:16 -0300

    ajuste em environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://microstack-j-1.onrender.com/api'  // URL do backend no Render
+  apiUrl: 'https://microstack-j-front.onrender.com/api'  // URL do backend no Render
 };
-
